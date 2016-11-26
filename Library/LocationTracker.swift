43ebcc8159f93a28176a8b65ba2b05caf7772fe8
Elvis Nuñez <elvisnunez@me.com>
2016-11-26 15:39:32 +0100

    Refactor

---
+++ Library/LocationTracker.swift
@@ -0,0 +1,55 @@
+import CoreLocation
+
+protocol LocationTrackerDelegate: class {
+    func locationTracker(_ locationTracker: LocationTracker, didFailWith error: NSError)
+    func locationTracker(_ locationTracker: LocationTracker, didFindLocation placemark: CLPlacemark)
+}
+
+class LocationTracker: NSObject {
+    weak var delegate: LocationTrackerDelegate?
+
+    lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.delegate = self
+
+        return manager
+    }()
+
+    func checkAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse, .authorizedAlways:
+            self.locationManager.startUpdatingLocation()
+        case .notDetermined:
+            self.locationManager.requestWhenInUseAuthorization()
+        default: break
+        }
+    }
+}
+
+extension LocationTracker: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            self.locationManager.startUpdatingLocation()
+        case .denied, .restricted:
+            self.delegate?.locationTracker(self, didFailWith: NSError(domain: "no.bakkenbaeck.sol", code: 9999, userInfo: [NSLocalizedDescriptionKey: "Unauthorized to get location access"]))
+        default: break
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard locations.count > 0 else { return }
+
+        self.locationManager.stopUpdatingLocation()
+
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(locations.first!) { placemarks, error in
+            if let error = error {
+                self.delegate?.locationTracker(self, didFailWith: error as NSError)
+            } else if let placemarks = placemarks {
+                let placemark = placemarks.first!
+                self.delegate?.locationTracker(self, didFindLocation: placemark)
+            }
+        }
+    }
+}
--- iOS/Source/MainController.swift
+++ iOS/Source/MainController.swift
@@ -2,11 +2,8 @@ import UIKit
 import CoreLocation
 
 class MainController: UIViewController {
-    lazy var locationManager: CLLocationManager = {
-        let manager = CLLocationManager()
-        manager.delegate = self
-
-        return manager
+    lazy var locationTracker: LocationTracker = {
+        return LocationTracker()
     }()
 
     lazy var descriptionLabel: UILabel = {
@@ -31,14 +28,6 @@ class MainController: UIViewController {
         self.view.backgroundColor = .white
         self.view.addSubview(self.backgroundImageView)
         self.view.addSubview(self.descriptionLabel)
-
-        switch CLLocationManager.authorizationStatus() {
-        case .authorizedWhenInUse, .authorizedAlways:
-            self.locationManager.startUpdatingLocation()
-        case .notDetermined:
-            self.locationManager.requestWhenInUseAuthorization()
-        default: break
-        }
     }
 
     override func viewWillLayoutSubviews() {
@@ -51,42 +40,26 @@ class MainController: UIViewController {
     }
 }
 
-extension MainController: CLLocationManagerDelegate {
-    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        switch status {
-        case .authorizedWhenInUse, .authorizedAlways:
-            self.locationManager.startUpdatingLocation()
-        case .denied, .restricted:
-            self.descriptionLabel.text = "We need to know where you are, enable location access in your Settings."
-        default: break
-        }
+extension MainController: LocationTrackerDelegate {
+    func locationTracker(_ locationTracker: LocationTracker, didFailWith error: NSError) {
+        self.descriptionLabel.text = "We need to know where you are, enable location access in your Settings."
+
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
     }
 
-    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard locations.count > 0 else { return }
-
-        self.locationManager.stopUpdatingLocation()
-
-        let geocoder = CLGeocoder()
-        geocoder.reverseGeocodeLocation(locations.first!) { placemarks, error in
-            if let error = error {
-                let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
-                self.present(alertController, animated: true, completion: nil)
-            } else if let placemarks = placemarks {
-                let placemark = placemarks.first!
-                let city = placemark.locality!
-                let interval = APIClient.dayLenghtDifference(for: placemark)
-                let minutes = interval / 60
-                let formatter = NumberFormatter()
-                formatter.maximumFractionDigits = 2
-                let minutesString = formatter.string(from: NSNumber(value: abs(minutes)))!
-                if minutes > 0 {
-                    self.descriptionLabel.text = "Today, \(city) has \n\(minutesString) minutes more sunlight than yesterday. Enjoy!"
-                } else {
-                    self.descriptionLabel.text = "Today, \(city) has \n\(minutesString) minutes less sunlight than yesterday. Sorry."
-                }
-            }
+    func locationTracker(_ locationTracker: LocationTracker, didFindLocation placemark: CLPlacemark) {
+        let city = placemark.locality!
+        let interval = APIClient.dayLenghtDifference(for: placemark)
+        let minutes = interval / 60
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 2
+        let minutesString = formatter.string(from: NSNumber(value: abs(minutes)))!
+        if minutes > 0 {
+            self.descriptionLabel.text = "Today, \(city) has \n\(minutesString) minutes more sunlight than yesterday. Enjoy!"
+        } else {
+            self.descriptionLabel.text = "Today, \(city) has \n\(minutesString) minutes less sunlight than yesterday. Sorry."
         }
     }
 }
