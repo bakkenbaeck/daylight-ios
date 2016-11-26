b92cad461aa549a289288d85e73fdfe0b0181331
Elvis Nuñez <elvisnunez@me.com>
2016-11-26 15:50:11 +0100

    Use AutoLayout

--- iOS/Source/MainController.swift
+++ iOS/Source/MainController.swift
@@ -15,12 +15,14 @@ class MainController: UIViewController {
         label.numberOfLines = 0
         label.font = .bold(size: 40)
         label.textColor = .messageColor
+        label.translatesAutoresizingMaskIntoConstraints = false
 
         return label
     }()
 
     lazy var backgroundImageView: UIImageView = {
         let view = UIImageView(image: UIImage(named: "background")!)
+        view.translatesAutoresizingMaskIntoConstraints = false
 
         return view
     }()
@@ -28,20 +30,24 @@ class MainController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = .white
-        self.view.addSubview(self.backgroundImageView)
-        self.view.addSubview(self.descriptionLabel)
-
+        self.addSubviewsAndContrains()
         self.locationTracker.checkAuthorization()
     }
 
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
+    func addSubviewsAndContrains() {
+        self.view.addSubview(self.backgroundImageView)
+        self.view.addSubview(self.descriptionLabel)
 
-        self.backgroundImageView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        self.backgroundImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
+        self.backgroundImageView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0).isActive = true
+        self.backgroundImageView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0).isActive = true
+        self.backgroundImageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
 
-        let margin = CGFloat(30)
-        self.descriptionLabel.frame = CGRect(x: margin, y: 0, width: self.view.frame.width - margin * 2, height: self.view.frame.height)
+        let insets = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
+        self.descriptionLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: insets.top).isActive = true
+        self.descriptionLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: insets.left).isActive = true
+        self.descriptionLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -insets.right).isActive = true
+        self.descriptionLabel.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -insets.bottom).isActive = true
     }
 }
 
