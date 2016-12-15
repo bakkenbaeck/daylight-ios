6f1f72f081346cffe9012b3a68119a91a1833fc6
marijnschilling <marijnschilling@gmail.com>
2016-12-15 17:03:22 +0100

    Add sweetUIKit with carthage

---
+++ Carthage/Build/iOS/SweetUIKit.framework/Headers/Identifiable.swift
@@ -0,0 +1,25 @@
+#if os(iOS) || os(tvOS)
+    import UIKit
+
+    public protocol Identifiable {
+        static var reuseIdentifier: String { get }
+    }
+
+    public extension Identifiable {
+        public static var reuseIdentifier: String {
+            get {
+                return String(describing: self)
+            }
+        }
+    }
+
+    extension UITableViewCell: Identifiable {
+    }
+
+    extension UITableViewHeaderFooterView: Identifiable {
+    }
+
+    extension UICollectionReusableView: Identifiable {
+
+    }
+#endif
