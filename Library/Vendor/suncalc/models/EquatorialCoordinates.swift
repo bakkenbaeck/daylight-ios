0990275dc84a62fd9ac47c34f22f4f6790901793
Elvis Nuñez <elvisnunez@me.com>
2016-12-19 13:30:58 +0100

    Add helper to calculate sunphases

---
+++ Library/Vendor/suncalc/models/EquatorialCoordinates.swift
@@ -0,0 +1,19 @@
+//
+//  EquatorialCoordinates.swift
+//  suncalc-example
+//
+//  Created by Shaun Meredith on 10/2/14.
+//  Copyright (c) 2014 Chimani, LLC. All rights reserved.
+//
+
+import Foundation
+
+class EquatorialCoordinates {
+    var rightAscension: Double
+    var declination: Double
+
+    init(rightAscension: Double, declination: Double) {
+        self.rightAscension = rightAscension
+        self.declination = declination
+    }
+}
