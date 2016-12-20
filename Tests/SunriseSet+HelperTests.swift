e0f4e772109384d37837b1e8a02fabf5a86f7751
Elvis Nuñez <elvisnunez@me.com>
2016-12-20 12:57:32 +0100

    Fix tests

--- Tests/SunriseSet+HelperTests.swift
+++ Tests/SunriseSet+HelperTests.swift
@@ -20,28 +20,28 @@ class Suntimes_HelperTests: XCTestCase {
         dateComponents.hour = 0
         dateComponents.minute = 0
         var date = calendar.date(from: dateComponents)!
-        var sunriseSet = Suntimes(date: date, timeZone: timeZone, latitude: osloCoordinate.latitude, longitude: osloCoordinate.longitude)
-        XCTAssertEqual(sunriseSet.daylightLengthProgress, 0)
+        var suntimes = Suntimes(date: date, timeZone: timeZone, latitude: osloCoordinate.latitude, longitude: osloCoordinate.longitude)
+        XCTAssertEqual(suntimes.daylightLengthProgress, 0)
 
         dateComponents.hour = 8
         dateComponents.minute = 18
         dateComponents.second = 04
         date = calendar.date(from: dateComponents)!
-        sunriseSet = Suntimes(date: date, timeZone: timeZone, latitude: osloCoordinate.latitude, longitude: osloCoordinate.longitude)
-        XCTAssertEqual(sunriseSet.daylightLengthProgress, 0)
+        suntimes = Suntimes(date: date, timeZone: timeZone, latitude: osloCoordinate.latitude, longitude: osloCoordinate.longitude)
+        XCTAssertEqual(suntimes.daylightLengthProgress, 0)
 
         dateComponents.hour = 11
         dateComponents.minute = 11
         dateComponents.second = 45
         date = calendar.date(from: dateComponents)!
-        sunriseSet = Suntimes(date: date, timeZone: timeZone, latitude: osloCoordinate.latitude, longitude: osloCoordinate.longitude)
-        XCTAssertEqual(sunriseSet.daylightLengthProgress, 0.5)
+        suntimes = Suntimes(date: date, timeZone: timeZone, latitude: osloCoordinate.latitude, longitude: osloCoordinate.longitude)
+        XCTAssertEqual(suntimes.daylightLengthProgress, 0.5)
 
         dateComponents.hour = 14
         dateComponents.minute = 23
         dateComponents.second = 15
         date = calendar.date(from: dateComponents)!
-        sunriseSet = Suntimes(date: date, timeZone: timeZone, latitude: osloCoordinate.latitude, longitude: osloCoordinate.longitude)
-        XCTAssertEqual(sunriseSet.daylightLengthProgress, 1)
+        suntimes = Suntimes(date: date, timeZone: timeZone, latitude: osloCoordinate.latitude, longitude: osloCoordinate.longitude)
+        XCTAssertEqual(suntimes.daylightLengthProgress, 1)
     }
 }
