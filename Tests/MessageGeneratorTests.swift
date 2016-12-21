6493f8fc5a54137d761ce5131dfcc8c51e1f7bc8
marijnschilling <marijnschilling@gmail.com>
2016-12-21 11:28:59 +0100

    Update tests

--- Tests/MessageGeneratorTests.swift
+++ Tests/MessageGeneratorTests.swift
@@ -5,12 +5,12 @@ class MessageGeneratorTests: XCTestCase {
     func testDatesOfComingYear() {
         let date = Date()
 
-        let datesOfComingYear: Array = date.datesOfComingYear()
+        let datesOfComingYear: Array = date.futureDates()
 
         let firstDay = datesOfComingYear.first
         let lastDay = datesOfComingYear.last
 
-        let dayInAYear = Calendar.current.date(byAdding: .day, value: 365, to: date)
+        let dayInAYear = Calendar.current.date(byAdding: .day, value: 30, to: date)
 
         XCTAssertEqual(date, firstDay)
         XCTAssertEqual(dayInAYear, lastDay)
