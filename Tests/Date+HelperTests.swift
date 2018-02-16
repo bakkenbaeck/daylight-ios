5704463330e04e724722c964855860339641eea3
Igor Ranieri Elland <igor@elland.me>
2018-02-16 11:58:23 +0100

    Updated dates and calendars b/c time zone.

--- Tests/Date+HelperTests.swift
+++ Tests/Date+HelperTests.swift
@@ -5,14 +5,16 @@ import XCTest
 class Date_HelperTests: XCTestCase {
 
     func testNext30Dates() {
-        let date = Date()
+        let calendar = Calendar.autoupdatingCurrent
+        let components = DateComponents(calendar: calendar, timeZone: TimeZone(secondsFromGMT: 0), year: 2018, month: 1, day: 1, hour: 1, minute: 1, second: 1)
+        let date = calendar.date(from: components)!
 
         let dateWithNext30: Array = date.andNext30Days()
 
         let firstDay = dateWithNext30.first
         let lastDay = dateWithNext30.last
 
-        let day31 = Calendar.autoupdatingCurrent.date(byAdding: .day, value: 30, to: date)
+        let day31 = calendar.date(byAdding: .day, value: 30, to: date)
 
         XCTAssertEqual(date, firstDay)
         XCTAssertEqual(day31, lastDay)
