b0b28347021708297f76354de3be2c9196ed2461
marijnschilling <marijnschilling@gmail.com>
2018-09-24 16:59:32 +0200

    Fix syntax changes

--- iOS/Source/AppDelegate.swift
+++ iOS/Source/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder {
 
 extension AppDelegate: UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         guard let window = self.window else { fatalError("Window not found") }
 
