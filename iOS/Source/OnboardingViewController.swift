b82c8cb1c40f83600a9fc3346b268e13495b4b3b
wesleybb <wesley@bakkenbaeck.no>
2021-03-20 11:46:10 +0100

    Add fullScreen modal presentation

--- iOS/Source/OnboardingViewController.swift
+++ iOS/Source/OnboardingViewController.swift
@@ -129,6 +129,7 @@ class OnboardingViewController: UIViewController {
 
         let mainController = MainViewController(with: daylightController)
         mainController.modalTransitionStyle = .crossDissolve
+        mainController.modalPresentationStyle = .fullScreen
         self.present(mainController, animated: true)
     }
 
